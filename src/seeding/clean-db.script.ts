b5d309779491dcf79b27a8fa7bc5f341bd2e14fc
Santiago Herrera <santiherreravelas@gmail.com>
2024-01-09 15:15:11 -0500

    fix: :bug: clean script fixing

--- src/seeding/clean-db.script.ts
+++ src/seeding/clean-db.script.ts
@@ -4,10 +4,12 @@ const dataSource = ormconfig
 
 dataSource.initialize().then(async () =>{
     //clean db
-    await dataSource.query("DELETE FROM user")
-    await dataSource.query("DELETE FROM payment")
-    await dataSource.query("DELETE FROM payment_source")
-    await dataSource.query("DELETE FROM ride")
+    await dataSource.query('SET CONSTRAINTS ALL DEFERRED')
+    await dataSource.query('DELETE FROM "payment"')
+    await dataSource.query('DELETE FROM "user"')
+    await dataSource.query('DELETE FROM "payment_source"')
+    await dataSource.query('DELETE FROM "ride"')
+    await dataSource.query('SET CONSTRAINTS ALL IMMEDIATE')
 
     console.log("Data base cleaned successfully")
     process.exit()
